ecb491a60b77bb13a96caef9f4ef00d77c8cc93e
Overdjosed <jose.ancizar.667@gmail.com>
2024-07-26 19:06:20 +0200

    Actualizando cambios de estilos desde otra rama

--- components/shared/WrapView.tsx
+++ components/shared/WrapView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 
 interface Props {
@@ -8,26 +8,26 @@ interface Props {
   verticalSpacing?: number;
 }
 
-export default function WrapView({
-  children,
-  horizontalSpacing = 0,
-  verticalSpacing = 0,
-  style = {}
+export default function WrapView({ 
+  children, 
+  horizontalSpacing = 0, 
+  verticalSpacing = 0, 
+  style = {} 
 }: Props) {
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      marginTop: 20, // Cambiado por José
-      marginHorizontal: -horizontalSpacing / 2, // Negative margin to offset the padding added to each item
-      marginVertical: -verticalSpacing / 2, // Negative margin to offset the padding added to each item
-      justifyContent: 'center', // Cambiado por José
-    },
-    item: {
-      marginHorizontal: horizontalSpacing / 2,
-      marginVertical: verticalSpacing / 2,
-    },
-  });
+  const styles = useRef(
+      StyleSheet.create({
+      container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginHorizontal: -horizontalSpacing / 2, // Negative margin to offset the padding added to each item
+        marginVertical: -verticalSpacing / 2, // Negative margin to offset the padding added to each item
+      },
+      item: {
+        marginHorizontal: horizontalSpacing / 2,
+        marginVertical: verticalSpacing / 2,
+      },
+    })
+  ).current;
 
   return (
     <View style={[styles.container, style]}>
