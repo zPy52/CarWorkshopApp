8c0426a667a0c2e34d7fa2689368acab7c98871f
Overdjosed <jose.ancizar.667@gmail.com>
2024-07-27 22:08:31 +0200

    Trayendo static_images.ts de RamaJose a DefaultBranch

--- constants/static_images.ts
+++ constants/static_images.ts
@@ -10,6 +10,15 @@ interface StaticImagesType {
   real: {
     [key: string]: ImageSourcePropType
   };
+  other: {
+    [key: string]: ImageSourcePropType
+  };
+  carTypes: {
+    [key: string]: ImageSourcePropType
+  };
+  kitImages: {
+    [key: string]: ImageSourcePropType
+  };
 }
 
 const StaticImages: StaticImagesType = {
@@ -19,12 +28,39 @@ const StaticImages: StaticImagesType = {
     carRepair: require('../assets/images/icons/car-repair.png'),
     engineOil: require('../assets/images/icons/engine-oil.png'),
     shockAbsorbers: require('../assets/images/icons/shock-absorbers.png'),
+    user: require("../assets/images/icons/Buser.png"),
   },
   detailedIcons: {
     wheel: require('../assets/images/detailed_icons/wheel.png'),
+    tyre:  require('../assets/images/detailed_icons/tyre.png'),
+    tyreStack:  require('../assets/images/detailed_icons/tyreStack.png'),
+    oil:  require('../assets/images/detailed_icons/oil.png'),
+    shockAbsorber:  require('../assets/images/detailed_icons/shockAbsorber.png'),
+    timingBelt:  require('../assets/images/detailed_icons/timingBelt.png'),
   },
   real: {
     tyre: require('../assets/images/real/tyre.png'),
+  },
+  other: {
+    pr1: require("../assets/images/promoexamples/promo1prueba.png"),
+    pr2: require("../assets/images/promoexamples/promo2prueba.png"),
+    pr3: require("../assets/images/promoexamples/promo3prueba.png"),
+    pr4: require("../assets/images/promoexamples/promo4prueba.png"),
+  },
+  carTypes: {
+    allTerrain: require("../assets/images/CarTypes/allTerrain.png"),
+    boxTruck: require("../assets/images/CarTypes/boxTruck.png"),
+    car: require("../assets/images/CarTypes/car.png"),
+    raceCar: require("../assets/images/CarTypes/raceCarBw.png"),
+    raceCarColoured: require("../assets/images/CarTypes/raceCarColoured.png"),
+    scarabTypeShit: require("../assets/images/CarTypes/scarabTypeShit.png"),
+    taxi: require("../assets/images/CarTypes/taxi.png"),
+    van: require("../assets/images/CarTypes/van.png")
+  },
+  kitImages: {
+    distrIm: require("../assets/images/kits/distr.jpg"),
+    brakeIm: require("../assets/images/kits/brake2.jpg"),
+    ITVIm: require("../assets/images/kits/itv.jpg"),
   }
 };
 
