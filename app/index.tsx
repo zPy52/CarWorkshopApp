65b5b0dd7bcb724001ceb4539f6f98698adbcbe2
Antonio Peña Peña <antonio.pennap@estudiante.uam.es>
2024-07-30 17:37:49 +0100

    Removing any references to redux

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,6 @@ import React, { useEffect } from "react";
 import { useTheme } from "../hooks/theme";
 import Insets from "../constants/insets";
 import Clickable from "../components/shared/Clickable";
-import { Provider } from "react-redux";
-import store from "../manager/store";
 import StaticImages from "../constants/static_images";
 
 // To run in Expo Go: npx expo start --tunnel
@@ -34,48 +32,46 @@ export default function App() {
   }, []);
 
   return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <View
-          style={{
-            marginLeft: Insets.screenMarginMedium,
-            marginTop: Insets.screenMarginLarge,
-            alignItems: "flex-start",
+    <SafeAreaView>
+      <View
+        style={{
+          marginLeft: Insets.screenMarginMedium,
+          marginTop: Insets.screenMarginLarge,
+          alignItems: "flex-start",
+        }}
+      >
+        <Clickable
+          onPress={() => {
+            router.navigate("/colors");
           }}
+          style={{ alignItems: "center" }}
         >
-          <Clickable
-            onPress={() => {
-              router.navigate("/colors");
-            }}
-            style={{ alignItems: "center" }}
-          >
-            <Text style={{ color: theme.colors.onBackground }}>
-              Ver color roles
-            </Text>
-          </Clickable>
-          <View style={{ height: 20 }}></View>
-          <Clickable
-            onPress={() => {
-              router.navigate("/home");
-            }}
-            style={{ alignItems: "center" }}
-          >
-            <Text style={{ color: theme.colors.onBackground }}>
-              Navega a la home
-            </Text>
-          </Clickable>
-          <Clickable
-            onPress={() => {
-              router.navigate("/onboarding");
-            }}
-            style={[{ paddingTop: 20 }, { alignItems: "center" }]}
-          >
-            <Text style={{ color: theme.colors.onBackground }}>
-              Navega al onboarding
-            </Text>
-          </Clickable>
-        </View>
-      </SafeAreaView>
-    </Provider>
+          <Text style={{ color: theme.colors.onBackground }}>
+            Ver color roles
+          </Text>
+        </Clickable>
+        <View style={{ height: 20 }}></View>
+        <Clickable
+          onPress={() => {
+            router.navigate("/home");
+          }}
+          style={{ alignItems: "center" }}
+        >
+          <Text style={{ color: theme.colors.onBackground }}>
+            Navega a la home
+          </Text>
+        </Clickable>
+        <Clickable
+          onPress={() => {
+            router.navigate("/onboarding");
+          }}
+          style={[{ paddingTop: 20 }, { alignItems: "center" }]}
+        >
+          <Text style={{ color: theme.colors.onBackground }}>
+            Navega al onboarding
+          </Text>
+        </Clickable>
+      </View>
+    </SafeAreaView>
   );
 }
